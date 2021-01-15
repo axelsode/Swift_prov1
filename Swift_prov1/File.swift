a2ae665798624ef9367099ee815a22f343421436
axelsode <axel.soderber1996@gmai.com>
2021-01-15 10:13:47 +0100

    push to GitHub

---
+++ Swift_prov1/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Swift_prov1
+//
+//  Created by Axel Söderberg on 2021-01-11.
+//
+
+import Foundation
--- Swift_prov1/ViewController.swift
+++ Swift_prov1/ViewController.swift
@@ -9,14 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var playerNow = 1 //Circul
+    var playerNow = 1 //1 = nought play pice 2 = cross game pice
     var game = [0,0,0,0,0,0,0,0,0] //list of all pieces
+    var gamePlaying = true
     
     let winnerCombination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
+    @IBAction func player1Label(_ sender: Any) {
+    }
+    @IBAction func player2Label(_ sender: Any) {
+    }
+    @IBOutlet weak var playerWinLabel: UILabel!
     @IBAction func action(_ sender: Any) {
         
-        if(game[(sender as AnyObject).tag-1] == 0)
+        if(game[(sender as AnyObject).tag-1] == 0 && gamePlaying == true)
         {
             game[(sender as AnyObject).tag-1] = playerNow //only 1 press
             
@@ -31,7 +37,61 @@ class ViewController: UIViewController {
             }
         }
         
+        for comb in winnerCombination {
+            if game[comb[0]] != 0 && game[comb[0]] == game[comb[1]] && game[comb[1]] == game[comb[2]]
+            {
+                gamePlaying = false
+                
+                if game[comb[0]] == 1{
+                    // nought win
+                    playerWinLabel.text = "Nought has won"
+                    
+                }
+                else {
+                    //cross win
+                    playerWinLabel.text = "Cross has won"
+                }
+                
+                restartButton.isHidden = false
+                playerWinLabel.isHidden = false
+                
+            }
+        }
+        
+        gamePlaying = false
+        
+        for i in game{
+            if i == 0
+            {
+                gamePlaying = true
+                break
+            }
+        }
+        if gamePlaying == false
+        {
+            playerWinLabel.text = "It was a draw"
+            playerWinLabel.isHidden = false
+            restartButton.isHidden = false
+        }
     }
+    
+    @IBOutlet weak var restartButton: UIButton!
+    @IBAction func restart(_ sender: Any) {
+        
+        game = [0,0,0,0,0,0,0,0,0]
+        gamePlaying = true
+        playerNow = 1
+        
+        playerWinLabel.isHidden = true
+        restartButton.isHidden = true
+        
+        for i in 1...9 {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControl.State())
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
